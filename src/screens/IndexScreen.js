7e58e4cdd43eb6b5a28fcb6964fc1eab7b39131c
Jonny Graybill <jon.a.graybill@gmail.com>
2020-06-18 18:42:54 -0700

    add functionality via reducer

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+  
+  return <View>
+    <Text>Index Screen</Text>
+    <Button title="Add Post" onPress={addBlogPost}/>
+    <FlatList
+      data={state}
+      keyExtractor={(blogPost) =>blogPost.title}
+      // line below - item is equivalent to our individual blog post objects
+      renderItem={({ item }) => {
+        return <Text>{item.title}</Text>
+      }}
+    />
+  </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
